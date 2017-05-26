504f58169c9c7a846d1e0ab161a15929ab2bf50c
kmart22 <kmar7070@740s-pl-111.local>
2017-05-26 09:36:21 -0600

    created program for stack and sue assignment

---
+++ src/controller/StructureController.java
@@ -0,0 +1,41 @@
+package controller;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+import model.Customer;
+
+public class StructureController 
+{
+	private Stack<String> stringStack;
+	private Queue<Customer> customerQueue;
+	
+	public StructureController() 
+	{
+		stringStack = new Stack<String>();
+		customerQueue = new LinkedList<Customer>();
+	}
+	
+	public void start()
+	{
+		// STACK
+		stringStack.push("Hello");
+		stringStack.push("Not world");
+		
+		String temp = stringStack.peek();
+		System.out.println(temp);
+		
+		stringStack.pop();
+		
+		String temp2 = stringStack.peek();
+		System.out.println(temp);
+		
+		// QUEUE
+		customerQueue.add(new Customer("Billy"));
+		customerQueue.add(new Customer("Bob"));
+		customerQueue.add(new Customer("Joe"));
+		customerQueue.remove();
+		
+	}
+}
---
+++ src/controller/StructureRunner.java
@@ -0,0 +1,10 @@
+package controller;
+
+public class StructureRunner 
+{
+	public static void main(String [] args) 
+	{
+		StructureController myController = new StructureController();
+		myController.start();
+	}
+}
